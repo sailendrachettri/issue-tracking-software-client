954d55c76bfba406b3deca6dafc3954f8bc0248f
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-05-12 22:36:25 +0530

    latest changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './areas/public/homepage/homepage.component';
-import { ContactsDashboardComponent } from './areas/auth/modules/contacts-dashboard/contacts-dashboard.component';
-import { NewsletterDashboardComponent } from './areas/auth/modules/newsletter-dashboard/newsletter-dashboard.component';
 import { SystemAdminHomeComponent } from './areas/auth/system-admin/system-admin-home/system-admin-home.component';
 
 
@@ -22,12 +20,5 @@ export const routes: Routes = [
     path: 'auth/role/system-admin',
     component: SystemAdminHomeComponent,
   },
-  {
-    path: 'admin/cms/newsletter',
-    component: NewsletterDashboardComponent,
-  },
-  {
-    path: 'admin/cms/contacts',
-    component: ContactsDashboardComponent,
-  },
+
 ];
