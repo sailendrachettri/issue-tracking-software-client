b441dd53a4c8b44575d69c6c610de5222a078a09
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-05-25 17:46:37 +0530

    prev changes

--- src/app/areas/auth/system-admin/system-admin-home/system-admin-home.component.ts
+++ src/app/areas/auth/system-admin/system-admin-home/system-admin-home.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardComponent } from '../../dashboard-components/dashboard/dashboard.component';
-// import { WorkOnProgressComponent } from 'src/utilities/work-on-progress/work-on-progress.component';
-import { AxiosService } from 'src/app/api/axios.service';
-import { GET_DASHBOARD_DETAILS_URL } from 'src/app/api/api_routing_urls';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-system-admin-home',
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, NgFor],
   templateUrl: './system-admin-home.component.html',
   styleUrl: './system-admin-home.component.css',
 })
 export class SystemAdminHomeComponent implements OnInit {
-  underDevelopment: string = 'assets/gifs/dev.gif';
 
-  constructor(private axiosService: AxiosService) {}
 
   dashboardDetails: any;
+  tableData: any = [];
   async ngOnInit() {
-    const response = await this.axiosService.axiosInstance(
-      GET_DASHBOARD_DETAILS_URL
-    );
+   
 
-    console.log(response.data);
-    this.dashboardDetails = response.data;
+    this.tableData = [
+      {
+        id: 1,
+        name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        priority: 'High',
+        status: 'Pending',
+        openedDate: '10-05-2025',
+        daysOld: 15,
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, numquam? Culpa eum quae est autem...',
+        attachments: 'Attachments',
+        createdBy: 'Sailendra',
+        assignTo: 'Coder',
+        notificationStatus: 'Checked',
+      }
+    ];
+
+  
   }
 }
